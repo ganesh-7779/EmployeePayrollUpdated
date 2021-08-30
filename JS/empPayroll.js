501bb62cca3d5c33f41acbd98b3ae0b2a1ffa6f4
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-08-30 16:00:15 +0530

    Added : UC4 local Storage

--- JS/empPayroll.js
+++ JS/empPayroll.js
@@ -26,6 +26,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const save = () =>{
     try{
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
+
     }
     catch(e){
         return;
@@ -67,4 +69,15 @@ const getInputValueById = (id) => {
 const getInputElementValue = (id) => {
     let value = document.getElementById(id).value;
     return value;
+}
+//UC4
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList"), JSON.stringify(employeePayrollList)
 }
